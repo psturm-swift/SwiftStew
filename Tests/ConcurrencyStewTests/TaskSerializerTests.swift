dc2972f7fa7704613f109f89954e88d3621bc1e7
Patrick Sturm <psturm-swift@e.mail.de>
2021-10-23 00:46:06 +0200

    [impl] Bring test of `TaskSerializer` closer to real use case

--- Tests/ConcurrencyStewTests/TaskSerializerTests.swift
+++ Tests/ConcurrencyStewTests/TaskSerializerTests.swift
@@ -2,86 +2,76 @@ import XCTest
 @testable import ConcurrencyStew
 
 final class TaskSerializerTests: XCTestCase {
-    func testTasksAreExecutedInOrder() async {
-        let taskSerizalizer = TaskSerializer()
-        let resultCollector = ResultCollector()
+    func testTasksAreExecutedInOrderOnANonReentrantActor() async {
+        let actor = NonReentrantActor()
 
-        let t1 = Task.detached {
-            await taskSerizalizer.execute {
-                await resultCollector.add(index: 0)
-                await Task.sleep(1_000_000_000)
-                await resultCollector.add(index: 1)
-            }
-        }
-        
-        let t2 = Task.detached {
-            await taskSerizalizer.execute {
-                await resultCollector.add(index: 2)
-                await Task.sleep(10_000_000)
-                await resultCollector.add(index: 3)
-            }
-        }
-
-        let t3 = Task.detached {
-            await taskSerizalizer.execute {
-                await resultCollector.add(index: 4)
-                await Task.sleep(1_000)
-                await resultCollector.add(index: 5)
-            }
-        }
+        let t1 = Task.detached { await actor.add(index1: 0, index2: 1, milliseconds: 1_000) }
+        let t2 = Task.detached { await actor.add(index1: 2, index2: 3, milliseconds: 100) }
+        let t3 = Task.detached { await actor.add(index1: 4, index2: 5, milliseconds: 10) }
 
         let _ = await t1.result
         let _ = await t2.result
         let _ = await t3.result
         
-        let inOrder = await resultCollector.isInOrder()
+        let inOrder = await actor.result.isInOrder()
         
         XCTAssertTrue(inOrder)
     }
     
-    func testTasksAreNotExecutedInOrderWithoutSerializer() async {
-        let resultCollector = ResultCollector()
+    func testTasksAreNotExecutedInOrderOnAReentrantActor() async {
+        let actor = ReentrantActor()
 
-        let t1 = Task.detached {
-            await resultCollector.add(index: 0)
-            await Task.sleep(1_000_000_000)
-            await resultCollector.add(index: 1)
-        }
-        
-        let t2 = Task.detached {
-            await resultCollector.add(index: 2)
-            await Task.sleep(10_000_000)
-            await resultCollector.add(index: 3)
-        }
-
-        let t3 = Task.detached {
-            await resultCollector.add(index: 4)
-            await Task.sleep(1_000)
-            await resultCollector.add(index: 5)
-        }
+        let t1 = Task.detached { await actor.add(index1: 0, index2: 1, milliseconds: 1_000) }
+        let t2 = Task.detached { await actor.add(index1: 2, index2: 3, milliseconds: 100) }
+        let t3 = Task.detached { await actor.add(index1: 4, index2: 5, milliseconds: 10) }
 
         let _ = await t1.result
         let _ = await t2.result
         let _ = await t3.result
         
-        let inOrder = await resultCollector.isInOrder()
+        let inOrder = await actor.result.isInOrder()
         
         XCTAssertFalse(inOrder)
     }
 }
 
-fileprivate actor ResultCollector {
+fileprivate actor NonReentrantActor {
+    private let taskSerializer = TaskSerializer()
     private(set) var result: [Int] = []
     
-    func add(index: Int) {
-        result.append(index)
+    func add(index1: Int, index2: Int, milliseconds: UInt64) async {
+        await taskSerializer.execute {
+            await self.addIndex(index: index1)
+            await Task.sleep(milliseconds * 1_000)
+            await self.addIndex(index: index2)
+        }
     }
     
+    func indices() async -> [Int] { result }
+
+    private func addIndex(index: Int) { result.append(index) }
+}
+
+fileprivate actor ReentrantActor {
+    private(set) var result: [Int] = []
+    
+    func add(index1: Int, index2: Int, milliseconds: UInt64) async {
+        self.addIndex(index: index1)
+        await Task.sleep(milliseconds * 1_000)
+        self.addIndex(index: index2)
+    }
+    
+    func indices() async -> [Int] { result }
+
+    private func addIndex(index: Int) { result.append(index) }
+}
+
+fileprivate extension Array where Element: Comparable {
     func isInOrder() -> Bool {
-        guard let first = result.first else { return true }
+        guard let first = first else { return true }
         var previous = first
-        
-        for current in result.dropFirst() {
+
+        for current in dropFirst() {
             if current <= previous {
                 return false
             }
