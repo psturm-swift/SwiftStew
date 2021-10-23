b1cd5598dfca69897f7163957a1794d124307e18
Patrick Sturm <psturm-swift@e.mail.de>
2021-10-23 19:14:29 +0200

    [refac] Remove constant `count`

--- Tests/ConcurrencyStewTests/AsyncThrowingUnfoldSequenceTests.swift
+++ Tests/ConcurrencyStewTests/AsyncThrowingUnfoldSequenceTests.swift
@@ -9,7 +9,6 @@ final class AsyncThrowingUnfoldSequenceTests: XCTestCase {
     func testSimpleAsyncThrowingUnfoldSequence() async throws {
         let delayMilliSeconds: UInt64 = 10
         let expectedResult = [0, 1, 2, 3, 4]
-        let count = expectedResult.count
         
         let timer = asyncSequence(first: 0, next: { i in
             await Task.sleep(delayMilliSeconds * 1_000_000)
@@ -18,13 +17,15 @@ final class AsyncThrowingUnfoldSequenceTests: XCTestCase {
         
         let start = Date().timeIntervalSince1970
         var actualValues: [Int] = []
-        for try await i in timer.prefix(count) {
+        for try await i in timer.prefix(expectedResult.count) {
             actualValues.append(i)
         }
         let end = Date().timeIntervalSince1970
         let durationMilliSeconds = UInt64(((end - start) * 1000.0).rounded())
 
         XCTAssertEqual(expectedResult, actualValues)
-        XCTAssertGreaterThanOrEqual(durationMilliSeconds, UInt64(count) * delayMilliSeconds)
+        XCTAssertGreaterThanOrEqual(
+            durationMilliSeconds,
+            UInt64(expectedResult.count) * delayMilliSeconds)
     }
 }
