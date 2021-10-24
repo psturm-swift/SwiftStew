86b81cc165e0633b22012ec24810eb4f5ec5278d
Patrick Sturm <psturm-swift@e.mail.de>
2021-10-24 15:33:46 +0200

    [impl] Add test case for Section.Policy == .cancelPreviousAction

--- Tests/ConcurrencyStewTests/AsyncSectionTests.swift
+++ Tests/ConcurrencyStewTests/AsyncSectionTests.swift
@@ -52,6 +52,31 @@ final class AsyncSectionTests: XCTestCase {
         XCTAssertTrue(inOrder)
     }
     
+    func testAllTasksAreCancelledButTheLast() async {
+        let actor = NonReentrantActor(policy: .cancelPreviousAction)
+
+        let t1 = Task.detached { try await actor.add(index1: 0, index2: 1, milliseconds: 1_000) }
+        let t2 = Task.detached { try await actor.add(index1: 2, index2: 3, milliseconds: 100) }
+        let t3 = Task.detached { try await actor.add(index1: 4, index2: 5, milliseconds: 10) }
+        let t4 = Task.detached { try await actor.add(index1: 6, index2: 7, milliseconds: 10) }
+
+        t2.cancel()
+        
+        let r1 = try? await t1.value
+        let r2 = try? await t2.value
+        let r3 = try? await t3.value
+        let r4 = try? await t4.value
+        
+        let inOrder = await actor.result.isInOrder()
+        
+        XCTAssertEqual(nil, r1)
+        XCTAssertEqual(nil, r2)
+        XCTAssertEqual(nil, r3)
+        XCTAssertEqual(7, r4)
+        XCTAssertTrue(inOrder)
+    }
+    
+
     func testTasksAreNotExecutedInOrderOnAReentrantActor() async {
         let actor = ReentrantActor()
 
@@ -77,8 +102,12 @@ final class AsyncSectionTests: XCTestCase {
 
 @available(iOS 15.0.0, macOS 12.0.0, tvOS 15.0.0, watchOS 8.0.0, *)
 fileprivate actor NonReentrantActor {
-    private let section = AsyncSection()
+    private let section: AsyncSection
     private(set) var result: [Int] = []
+    
+    init(policy: AsyncSection.Policy = .waitOnPreviousAction) {
+        self.section = AsyncSection(policy: policy)
+    }
     
     func add(index1: Int, index2: Int, milliseconds: UInt64) async throws -> Int {
         return try await section.execute {
