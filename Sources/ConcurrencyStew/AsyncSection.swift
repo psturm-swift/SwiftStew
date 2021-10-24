76efaedc0741f68a593b552f0eab2a3495dafbab
Patrick Sturm <psturm-swift@e.mail.de>
2021-10-24 15:21:39 +0200

    [impl] Add a policy to `AsyncSection`

--- Sources/ConcurrencyStew/AsyncSection.swift
+++ Sources/ConcurrencyStew/AsyncSection.swift
@@ -32,21 +32,35 @@
 ///  ```
 @available(iOS 15.0.0, macOS 12.0.0, tvOS 15.0.0, watchOS 8.0.0, *)
 public actor AsyncSection {
+    /// Describes the policy how `AsyncSection` should handle the previous action if a new one has been passed.
+    public enum Policy {
+        case cancelPreviousAction
+        case waitOnPreviousAction
+    }
+    
+    private let policy: Policy
     private var previousTask: AnyTask? = nil
     
     /// Constructs an instance of `AsyncSection`
-    public init() {}
+    /// - Parameters:
+    ///   - policy:Specifies  how function `execute(action:)` should handle the previous action
+    public init(policy: Policy = .waitOnPreviousAction) {
+        self.policy = policy
+    }
 
     /// Executes an action right after the previous action has been finished. This ensures that  one action after the other
-    ///  can be executed on ``AsyncSection``.
+    ///  can be executed on ``AsyncSection``. If `policy` is set to `Policy.cancelPreviousAction`, then
+    ///  the previous action will be cancelled before the new action will be started.
     ///
     /// - Parameters:
     ///   - action: Asynchronous function that should be executed. The function may throw and return a value.
     /// - Returns: The return value of `action`
     /// - Throws: The error thrown by `action`. Especially throws `CancellationError` if the parent task has been cancelled.
     public func execute<T>(action: @Sendable @escaping () async throws -> T) async throws -> T {
-        let previousTask = self.previousTask
-        let newTask = Task { () async throws -> T in
+        let newTask = Task { [policy, previousTask] () async throws -> T in
+            if policy == .cancelPreviousAction {
+                previousTask?.cancel()
+            }
             await previousTask?.completion()
             try Task.checkCancellation()
             return try await action()
