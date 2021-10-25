4c239d75da36d94da1e921045866d6519ac83888
Patrick Sturm <psturm-swift@e.mail.de>
2021-10-26 01:27:00 +0200

    [impl] Improve readibility

--- Tests/ConcurrencyStewTests/AsyncSectionTests.swift
+++ Tests/ConcurrencyStewTests/AsyncSectionTests.swift
@@ -9,10 +9,10 @@ final class AsyncSectionTests: XCTestCase {
     func testTasksAreExecutedInOrderOnANonReentrantActor() async {
         let actor = NonReentrantActor()
 
-        let t1 = Task.detached { try await actor.add(id: 1, milliseconds: 1_000) }
-        let t2 = Task.detached { try await actor.add(id: 3, milliseconds: 100) }
-        let t3 = Task.detached { try await actor.add(id: 5, milliseconds: 10) }
-        let t4 = Task.detached { try await actor.add(id: 7, milliseconds: 12) }
+        let t1 = Task.detached { try await actor.add(id1: 1, id2: 2, milliseconds: 1_000) }
+        let t2 = Task.detached { try await actor.add(id1: 3, id2: 4, milliseconds: 100) }
+        let t3 = Task.detached { try await actor.add(id1: 5, id2: 6, milliseconds: 10) }
+        let t4 = Task.detached { try await actor.add(id1: 7, id2: 8, milliseconds: 12) }
 
         let r1 = try? await t1.value
         let r2 = try? await t2.value
@@ -20,7 +20,11 @@ final class AsyncSectionTests: XCTestCase {
         let r4 = try? await t4.value
         
         let result = await actor.result
-        let nonInterleaved = result.nonInterleaved()
+        let nonInterleaved =
+            result.areConsecutive(id1: 1, id2: 2) &&
+            result.areConsecutive(id1: 3, id2: 4) &&
+            result.areConsecutive(id1: 5, id2: 6) &&
+            result.areConsecutive(id1: 7, id2: 8)
         
         XCTAssertEqual(1, r1)
         XCTAssertEqual(3, r2)
@@ -32,10 +36,10 @@ final class AsyncSectionTests: XCTestCase {
     func testTasksAreStillExecutedInOrderOnANonReentrantActorIfATaskInTheMiddleIsCancelled() async {
         let actor = NonReentrantActor()
 
-        let t1 = Task.detached { try await actor.add(id: 1, milliseconds: 1_000) }
-        let t2 = Task.detached { try await actor.add(id: 3, milliseconds: 100) }
-        let t3 = Task.detached { try await actor.add(id: 5, milliseconds: 10) }
-        let t4 = Task.detached { try await actor.add(id: 7, milliseconds: 12) }
+        let t1 = Task.detached { try await actor.add(id1: 1, id2: 2, milliseconds: 1_000) }
+        let t2 = Task.detached { try await actor.add(id1: 3, id2: 4, milliseconds: 100) }
+        let t3 = Task.detached { try await actor.add(id1: 5, id2: 6, milliseconds: 10) }
+        let t4 = Task.detached { try await actor.add(id1: 7, id2: 8, milliseconds: 12) }
 
         t2.cancel()
         
@@ -44,9 +48,12 @@ final class AsyncSectionTests: XCTestCase {
         let r3 = try? await t3.value
         let r4 = try? await t4.value
         
-        let result = await actor.result.filter { $0 != 3}
-        let nonInterleaved = result.nonInterleaved()
-        
+        let result = await actor.result
+        let nonInterleaved =
+            result.areConsecutive(id1: 1, id2: 2) &&
+            result.areConsecutive(id1: 5, id2: 6) &&
+            result.areConsecutive(id1: 7, id2: 8)
+
         XCTAssertEqual(1, r1)
         XCTAssertEqual(nil, r2)
         XCTAssertEqual(5, r3)
@@ -57,10 +64,10 @@ final class AsyncSectionTests: XCTestCase {
     func testAllTasksAreCancelledButOne() async {
         let actor = NonReentrantActor(policy: .cancelPreviousAction)
 
-        let t1 = Task.detached { try await actor.add(id: 1, milliseconds: 1_000) }
-        let t2 = Task.detached { try await actor.add(id: 3, milliseconds: 500) }
-        let t3 = Task.detached { try await actor.add(id: 5, milliseconds: 400) }
-        let t4 = Task.detached { try await actor.add(id: 7, milliseconds: 100) }
+        let t1 = Task.detached { try await actor.add(id1: 1, id2: 2, milliseconds: 1_000) }
+        let t2 = Task.detached { try await actor.add(id1: 3, id2: 4, milliseconds: 100) }
+        let t3 = Task.detached { try await actor.add(id1: 5, id2: 6, milliseconds: 10) }
+        let t4 = Task.detached { try await actor.add(id1: 7, id2: 8, milliseconds: 12) }
 
         let r1 = try? await t1.value
         let r2 = try? await t2.value
@@ -73,10 +80,10 @@ final class AsyncSectionTests: XCTestCase {
     func testTasksAreNotExecutedInOrderOnAReentrantActor() async {
         let actor = ReentrantActor()
         
-        let t1 = Task.detached { await actor.add(id: 1, milliseconds: 1_000) }
-        let t2 = Task.detached { await actor.add(id: 3, milliseconds: 100) }
-        let t3 = Task.detached { await actor.add(id: 5, milliseconds: 10) }
-        let t4 = Task.detached { await actor.add(id: 7, milliseconds: 12) }
+        let t1 = Task.detached { await actor.add(id1: 1, id2: 2, milliseconds: 1_000) }
+        let t2 = Task.detached { await actor.add(id1: 3, id2: 4, milliseconds: 100) }
+        let t3 = Task.detached { await actor.add(id1: 5, id2: 6, milliseconds: 10) }
+        let t4 = Task.detached { await actor.add(id1: 7, id2: 8, milliseconds: 12) }
 
         let r1 = await t1.value
         let r2 = await t2.value
@@ -84,8 +91,12 @@ final class AsyncSectionTests: XCTestCase {
         let r4 = await t4.value
         
         let result = await actor.result
-        let interleaved = !result.nonInterleaved()
-        
+        let interleaved =
+            !result.areConsecutive(id1: 1, id2: 2) ||
+            !result.areConsecutive(id1: 3, id2: 4) ||
+            !result.areConsecutive(id1: 5, id2: 6) ||
+            !result.areConsecutive(id1: 7, id2: 8)
+
         XCTAssertEqual(1, r1)
         XCTAssertEqual(3, r2)
         XCTAssertEqual(5, r3)
@@ -103,13 +114,13 @@ fileprivate actor NonReentrantActor {
         self.section = AsyncSection(policy: policy)
     }
     
-    func add(id: Int, milliseconds: UInt64) async throws -> Int {
+    func add(id1: Int, id2: Int, milliseconds: UInt64) async throws -> Int {
         return try await section.execute {
-            await self.add(index: id)
+            await self.add(index: id1)
             try await Task.sleep(nanoseconds: milliseconds * 1_000_000)
-            await self.add(index: id + 1)
+            await self.add(index: id2)
             
-            return id
+            return id1
         }
     }
 
@@ -122,12 +133,12 @@ fileprivate actor NonReentrantActor {
 fileprivate actor ReentrantActor {
     private(set) var result: [Int] = []
     
-    func add(id: Int, milliseconds: UInt64) async -> Int {
-        self.add(index: id)
+    func add(id1: Int, id2: Int, milliseconds: UInt64) async -> Int {
+        self.add(index: id1)
         await Task.sleep(milliseconds * 1_000_000)
-        self.add(index: id + 1)
+        self.add(index: id2)
 
-        return id
+        return id1
     }
     
     func indices() async -> [Int] { result }
@@ -136,13 +147,9 @@ fileprivate actor ReentrantActor {
 }
 
 fileprivate extension Array where Element == Int {
-    func nonInterleaved() -> Bool {
-        for i in stride(from: 0, to: count, by: 2) {
-            guard i + 1 < count else { return false }
-            if self[i] + 1 != self[i + 1] {
-                return false
-            }
-        }
-        return true
+    func areConsecutive(id1: Int, id2: Int) -> Bool {
+        guard let index1 = firstIndex(of: id1) else { return false }
+        guard let index2 = firstIndex(of: id2) else { return false }
+        return index1 + 1 == index2
     }
 }
